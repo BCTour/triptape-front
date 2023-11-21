c48a3cf0b225fdeb993c9b6a3cec6a1c86676da7
SoyeonKim <so0426yeon@gmail.com>
2023-11-21 17:51:41 +0900

    feat: 배너 표시 기능 구현중

--- triptape/src/components/banner/BannerCard.vue
+++ triptape/src/components/banner/BannerCard.vue
@@ -1,23 +1,35 @@
 <script setup>
 
 const props = defineProps({
-  bannerKey: Number,
   tape: Object,
   title: String,
   description: String,
+  imgSrc: String,
 })
 
+const src = "https://images.dog.ceo/breeds/terrier-australian/n02096294_7317.jpg";
 </script>
 
 <template>
-  <div class="card">
-    뭐
+  <div class="card" :style="{'background-image': `url(${imgSrc})`}">
+    <h1 class="text">{{ title }}</h1>
+    <p class="text">{{ description }}</p>
   </div>
 </template>
 
 <style scoped>
 .card {
   width: 100%;
-  background-color: aqua;
+  height: 400px;
+  padding: 32px;
+  display: flex;
+  justify-content: end;
+  box-sizing: border-box;
+  background-repeat : no-repeat;
+  background-size : cover;
+}
+.text {
+  color: white;
+  text-shadow: 2px 2px 5px rgba(0, 0, 0, 1);
 }
 </style>
